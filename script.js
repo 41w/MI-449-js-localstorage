430c85229844c865c03e51a35a42ab0d1111602a
41w <xusiyi@msu.edu>
2019-03-19 13:59:00 -0400

    Build a website allows the users to switch between themes and remembsers their choice

---
+++ script.js
@@ -0,0 +1,35 @@
+var changeTheme = function () {
+  var theme = window.localStorage.getItem('theme')
+  if (theme === null) {
+    document.body.setAttribute('class', 'day')
+    window.localStorage.setItem('theme', 'night')
+  } else {
+    if (theme === 'day') {
+      document.body.setAttribute('class', theme)
+      window.localStorage.setItem('theme', 'night')
+    } else {
+      document.body.setAttribute('class', theme)
+      window.localStorage.setItem('theme', 'day')
+    }
+  }
+}
+
+var theme = window.localStorage.getItem('theme')
+if (theme === 'day') {
+  document.body.setAttribute('class', 'night')
+}
+if (theme === 'night') {
+  document.body.setAttribute('class', 'day')
+}
+
+var button = document.getElementById('change')
+button.addEventListener('click', changeTheme)
+
+var counter = window.localStorage.getItem('counter')
+if (counter === null) {
+  counter = 1
+} else {
+  counter = parseInt(counter) + 1
+}
+window.localStorage.setItem('counter', counter)
+document.getElementById('text').textContent = 'Times you visited the website: ' + counter
